5c39663c9a4b2e8b795b036f9eb2a57c812962dc
Sourec <tothemyers@gmail.com>
2015-03-20 12:41:40 -0400

    Implemented all node stuff except Node::getEndType(), added /doxygen/ to the .gitignore

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,1 +1,125 @@
 #include "Node.h"
+
+int Node::count = 0;
+
+/** @brief Increments the tie count. Recursively increments the count for everything above it too.
+  * @param amount The amount to increment by. Can be negative.
+  */
+void Node::incrTieCount(int amount)
+{
+    tieCount += amount;
+    if (parent != nullptr)
+        parent->incrTieCount(amount);
+}
+
+/** @brief Increments the O win count. Recursively increments the count for everything above it too.
+  * @param amount The amount to increment by. Can be negative.
+  */
+void Node::incrOCount(int amount)
+{
+    oCount += amount;
+    if (parent != nullptr)
+        parent->incrOCount();
+}
+
+/** @brief Increments the X win count. Recursively increments the count for everything above it too.
+  * @param amount The amount to increment by. Can be negative.
+  */
+void Node::incrXCount(int amount)
+{
+    xCount += amount;
+    if (parent != nullptr)
+        parent->incrXCount();
+}
+
+/** @brief Gets the tie count
+  */
+uint8_t Node::getTieCount() const
+{
+    return tieCount;
+}
+
+/** @brief Gets the O win count.
+  */
+uint8_t Node::getOCount() const
+{
+    return oCount;
+}
+
+/** @brief Gets the X win count.
+  */
+uint8_t Node::getXCount() const
+{
+    return xCount;
+}
+
+/** @brief Gets what type of ending this board contains.
+  * @return Enumerated end type
+  */
+endType Node::getEndType() const
+{
+    return NONE;
+}
+
+/** @brief Solves for all of this node's children.
+  */
+void Node::solveForChildren()
+{
+    for (uint8_t iX = 0; iX < GRID_X; ++iX)
+    {
+        for (uint8_t iY = 0; iY < GRID_Y; ++iY)
+        {
+            //Check if this spot is already occupied.
+            if (board[iX][iY] != 0)
+                continue;
+
+            Node newNode;
+            newNode.parent = this;
+            newNode.turn = turn * -1;
+
+            //Copy this node's board over to the child node
+            for (uint8_t jX = 0; jX < GRID_X; ++jX)
+                for (uint8_t jY = 0; jY < GRID_Y; ++jY)
+                    newNode.board[jX][jY] = board[jX][jY];
+
+            //Apply the possible move.
+            newNode.board[iX][iY] = turn;
+
+            //Check this new node.
+            endType nodeType = newNode.getEndType();
+            if (nodeType == XWIN)
+                newNode.incrXCount();
+            else if (nodeType == OWIN)
+                newNode.incrOCount();
+            else
+                newNode.solveForChildren();
+
+            children.push_back(newNode);
+        }
+    }
+}
+
+/** @brief Returns the total count of all the node objects that have ever been created here.
+  */
+int Node::getCount()
+{
+    return count;
+}
+
+/** @brief Sets all values up, increments the global object count.
+  */
+ Node::Node()
+{
+    xCount = 0;
+    oCount = 0;
+    tieCount = 0;
+    turn = X;
+    parent = nullptr;
+    count++;
+
+    //Initialize board
+    for (uint8_t x = 0; x < GRID_X; ++x)
+        for (uint8_t y = 0; y < GRID_Y; ++y)
+            board[x][y] = 0;
+}
+
--- src/Node.h
+++ src/Node.h
@@ -8,8 +8,8 @@ const uint8_t GRID_X = 3;
 const uint8_t GRID_Y = 3;
 const uint8_t DIST_TO_WIN = 3; //The distance that a chain needs to cover in order to count as a win
 
-const uint8_t X = 1;
-const uint8_t O = 2;
+const uint8_t X = -1;
+const uint8_t O = 1;
 enum endType {NONE, XWIN, OWIN, TIE};
 
 
@@ -19,19 +19,21 @@ public:
     Node();
     void solveForChildren();
     endType getEndType() const;
+    static int getCount();
 
     uint8_t getXCount() const;
     uint8_t getOCount() const;
     uint8_t getTieCount() const;
-    void incrXCount();
-    void incrOCount();
-    void incrTieCount();
+    void incrXCount(int amount = 1);
+    void incrOCount(int amount = 1);
+    void incrTieCount(int amount = 1);
 protected:
     uint8_t xCount;
     uint8_t oCount;
     uint8_t tieCount;
+    uint8_t turn; //Whose turn it is RIGHT NOW. As in, ON THIS NODE!
 
-    Node* previous;
+    Node* parent;
     static int count;
 
     uint8_t board[GRID_X][GRID_Y];
